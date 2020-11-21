8243b7ec5568c2625aa1ba670358ec784933bac9
pecology <kobayashireo877@gmail.com>
2020-11-21 11:22:41 +0900

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    mode: 'development',
+    entry: './src/index.ts',
+    output: {
+      path: __dirname,
+      filename: './dist/bundle.js',
+    },
+    resolve: {
+      extensions: ['.ts', '.js']
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          loader: 'ts-loader'
+        }
+      ]
+    },
+    devServer: {
+        watchContentBase: true,
+        open:true
+    },
+    devtool: 'source-map'
+  }
